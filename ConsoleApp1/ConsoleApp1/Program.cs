ef527826bfc34d0b5e34b68a2d1b9b680d395076
FatimaAliyeva15 <bd7nyulpd@code.edu.az>
2024-03-09 23:11:06 +0400

    Conditions,Loops

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //int n = 123;
+            //int hasil = 1;
+            //int qaliq = 0;
+
+            //while(n != 0)
+            //{
+            //    qaliq = n % 10;
+            //    hasil *= qaliq;
+            //    n /= 10;
+
+            //}
+            //Console.WriteLine(hasil);
+            #endregion
+
+            #region Task2
+            //double n = 123;
+
+            //if(n < 2)
+            //{
+            //    Console.WriteLine("Eded ne sadedir ne de murekkeb");
+            //}
+            //else
+            //{
+            //    bool murekkeb_eded = false;
+            //    for (int i = 2; i <= Math.Sqrt(n); i++)
+            //    {
+            //        if (n % i == 0)
+            //        {
+            //            murekkeb_eded = true;
+            //            break;
+            //        }
+            //    }
+
+            //    if (murekkeb_eded)
+            //    {
+            //        Console.WriteLine("Eded murekkebdir");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Eded sadedir");
+            //    }
+            //}
+            #endregion
+
+            #region Task3
+            //int n = 4;
+
+            //if (n > 0)
+            //{
+            //    double faktorial = 1;
+
+            //    for(int i = 1; i <= n; i++)
+            //    {
+            //        faktorial *= i;
+            //    }
+            //    Console.WriteLine(faktorial);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eded menfi olmamalidir");
+            //}
+            #endregion
+
+            #region Task4
+            //int n = 1221;
+            //int ilkinhal = n;
+            //int qaliq = 0;
+            //int m = 0;
+
+            //while (n > 0)
+            //{
+            //    qaliq = n % 10;
+            //    m = m * 10 + qaliq;
+            //    n /= 10;
+            //}
+
+            //if (ilkinhal == m)
+            //{
+            //    Console.WriteLine("Eded polindromdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eded polindrom deyil");
+            //}
+            #endregion
+
+        }
+    }
+}
+
